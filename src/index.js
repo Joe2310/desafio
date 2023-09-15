57d640f684a427a962b5eb27a568c64337f857be
unknown <joe.ssa@hotmail.com>
2023-09-15 20:45:33 -0300

    Arquivo servidor adicionado

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-const express = require("express")
-const rotas = require('./rotas')
-const app = express()
-
-
-app.use(express.json())
-app.use(rotas)
+const app = require('./servidor')
 
 app.listen(3000)
